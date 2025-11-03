39c102ed371dcdc57ac958f0e8e31d46312d34f4
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2025-11-03 19:22:21 -0300

    Funções iniciais

---
+++ src/br/senai/sp/jandira/bhaskara/Bhaskara.java
@@ -0,0 +1,14 @@
+package br.senai.sp.jandira.bhaskara;
+
+import br.senai.sp.jandira.bhaskara.model.Usuario;
+
+public class Bhaskara {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        Usuario usuario = new Usuario();
+
+        usuario.obterValores();
+    }
+}
---
+++ src/br/senai/sp/jandira/bhaskara/model/Usuario.java
@@ -0,0 +1,13 @@
+package br.senai.sp.jandira.bhaskara.model;
+
+public class Usuario {
+
+    double x;
+    double a;
+    double b;
+    double c;
+
+    public void obterValores(){
+
+    }
+}
