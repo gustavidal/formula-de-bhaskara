8eefef8b8c32178ca34f1d9ec59cb62b360216c8
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2025-11-03 19:37:06 -0300

    Adição de função de obtenção de valores

--- src/br/senai/sp/jandira/bhaskara/model/Usuario.java
+++ src/br/senai/sp/jandira/bhaskara/model/Usuario.java
@@ -1,13 +1,27 @@
 package br.senai.sp.jandira.bhaskara.model;
 
+import java.util.Scanner;
+
 public class Usuario {
 
-    double x;
-    double a;
-    double b;
-    double c;
+    double x1;
+    double x2;
+    int a;
+    int b;
+    int c;
 
     public void obterValores(){
+        Scanner leitor = new Scanner(System.in);
 
+        System.out.print("Informe o valor de \"a\", aquele que acompanha o x²: ");
+        a = leitor.nextInt();
+
+        System.out.print("Informe o valor de \"b\", aquele que acompanha o x: ");
+        b = leitor.nextInt();
+
+        System.out.print("Informe o valor de \"c\", o termo independente: ");
+        c = leitor.nextInt();
+
+        System.out.println(a + b + c);
     }
 }
