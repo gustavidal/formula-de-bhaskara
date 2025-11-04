707fd890029a1b7170813da170f8bb240a6a3b53
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2025-11-03 21:56:05 -0300

    Adição de função que calcula os valores de x

--- src/br/senai/sp/jandira/bhaskara/model/Usuario.java
+++ src/br/senai/sp/jandira/bhaskara/model/Usuario.java
@@ -30,56 +30,77 @@ public class Usuario {
         int cPos = c * (-1);
 
         if (a == 0 && b == 0 && c == 0){
-            System.out.println("Sua afirmação é verdadeira!");
+            System.out.println("Afirmação verdadeira!");
+            System.out.println("0 = 0");
         } else if ((a == 0 && b == 0 && c > 0)
                 || (a == 0 && b == 0 && c < 0)){
-            System.out.println("Sua afirmação é falsa!");
+            System.out.println("Afirmação falsa!");
             System.out.println(c + " ≠ 0");
-        } else if ((a == 0 && b > 0 && c == 0)
-                || (a == 0 && b < 0 && c == 0)
-                || (a > 0 && b == 0 && c == 0)
-                || (a < 0 && b == 0 && c == 0)){
-            System.out.println("Problema sem solução: ∅ (vazio)");
+        } else if ((a == 0 && b > 0 && c == 0) || (a == 0 && b < 0 && c == 0)
+                || (a > 0 && b == 0 && c == 0) || (a < 0 && b == 0 && c == 0)){
+            System.out.println("Problema sem solução - ∅ (vazio)");
         } else if ((a == 0 && b > 0 && c > 0)
                 || (a == 0 && b < 0 && c > 0)){
             System.out.println("A equação é a seguinte:");
             System.out.println(b + "x + " + c + " = 0");
+            descobrirOsValoresDeX();
         } else if ((a == 0 && b > 0 && c < 0)
                 || (a == 0 && b < 0 && c < 0)){
             System.out.println("A equação é a seguinte:");
             System.out.println(b + "x - " + cPos + " = 0");
+            descobrirOsValoresDeX();
         } else if ((a > 0 && b == 0 && c > 0)
                 || (a < 0 && b == 0 && c > 0)){
             System.out.println("A equação é a seguinte:");
             System.out.println(a + "x² + " + c + " = 0");
+            descobrirOsValoresDeX();
         } else if ((a > 0 && b == 0 && c < 0)
                 || (a < 0 && b == 0 && c < 0)){
             System.out.println("A equação é a seguinte:");
             System.out.println(a + "x² - " + cPos + " = 0");
+            descobrirOsValoresDeX();
         } else if ((a > 0 && b > 0 && c == 0)
                 || (a < 0 && b > 0 && c == 0)){
             System.out.println("A equação é a seguinte:");
             System.out.println(a + "x² + " + b + "x = 0");
+            descobrirOsValoresDeX();
         } else if ((a > 0 && b > 0 && c > 0)
                 || (a < 0 && b > 0 && c > 0)){
             System.out.println("A equação é a seguinte:");
             System.out.println(a + "x² + " + b + "x + " + c + " = 0");
+            descobrirOsValoresDeX();
         } else if ((a > 0 && b > 0 && c < 0)
                 || (a < 0 && b > 0 && c < 0)){
             System.out.println("A equação é a seguinte:");
             System.out.println(a + "x² + " + b + "x - " + cPos + " = 0");
+            descobrirOsValoresDeX();
         } else if ((a > 0 && b < 0 && c == 0)
                 || (a < 0 && b < 0 && c == 0)){
             System.out.println("A equação é a seguinte:");
             System.out.println(a + "x² - " + bPos + "x" + " = 0");
+            descobrirOsValoresDeX();
         } else if ((a > 0 && b < 0 && c > 0)
                 || (a < 0 && b < 0 && c > 0)){
             System.out.println("A equação é a seguinte:");
             System.out.println(a + "x² - " + bPos + "x + " + c + " = 0");
+            descobrirOsValoresDeX();
         } else if ((a > 0 && b < 0 && c < 0)
                 || (a < 0 && b < 0 && c < 0)){
             System.out.println("A equação é a seguinte:");
             System.out.println(a + "x² - " + bPos + "x - " + cPos + " = 0");
+            descobrirOsValoresDeX();
         }
+    }
+
+    public void descobrirOsValoresDeX(){
+        double delta;
+        delta = (b * b) - (4 * a * c);
+
+
+        x1 = ((-b) + Math.sqrt(delta)) / (2 * a);
+        x2 = ((-b) - Math.sqrt(delta)) / (2 * a);
+
+        System.out.println(x1);
+        System.out.println(x2);
     }
 }
