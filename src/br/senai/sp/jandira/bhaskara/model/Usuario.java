935d2e85ebc5d22a5622ead952bd50f0031c402d
GUSTAVO VIDAL DE ABREU <gustavovidalgva@gmail.com>
2025-11-03 21:16:52 -0300

    Adição de função condicional com todas as possibilidades de equação

--- src/br/senai/sp/jandira/bhaskara/model/Usuario.java
+++ src/br/senai/sp/jandira/bhaskara/model/Usuario.java
@@ -22,6 +22,64 @@ public class Usuario {
         System.out.print("Informe o valor de \"c\", o termo independente: ");
         c = leitor.nextInt();
 
-        System.out.println(a + b + c);
+        formatarEquacao();
+    }
+
+    public void formatarEquacao(){
+        int bPos = b * (-1);
+        int cPos = c * (-1);
+
+        if (a == 0 && b == 0 && c == 0){
+            System.out.println("Sua afirmação é verdadeira!");
+        } else if ((a == 0 && b == 0 && c > 0)
+                || (a == 0 && b == 0 && c < 0)){
+            System.out.println("Sua afirmação é falsa!");
+            System.out.println(c + " ≠ 0");
+        } else if ((a == 0 && b > 0 && c == 0)
+                || (a == 0 && b < 0 && c == 0)
+                || (a > 0 && b == 0 && c == 0)
+                || (a < 0 && b == 0 && c == 0)){
+            System.out.println("Problema sem solução: ∅ (vazio)");
+        } else if ((a == 0 && b > 0 && c > 0)
+                || (a == 0 && b < 0 && c > 0)){
+            System.out.println("A equação é a seguinte:");
+            System.out.println(b + "x + " + c + " = 0");
+        } else if ((a == 0 && b > 0 && c < 0)
+                || (a == 0 && b < 0 && c < 0)){
+            System.out.println("A equação é a seguinte:");
+            System.out.println(b + "x - " + cPos + " = 0");
+        } else if ((a > 0 && b == 0 && c > 0)
+                || (a < 0 && b == 0 && c > 0)){
+            System.out.println("A equação é a seguinte:");
+            System.out.println(a + "x² + " + c + " = 0");
+        } else if ((a > 0 && b == 0 && c < 0)
+                || (a < 0 && b == 0 && c < 0)){
+            System.out.println("A equação é a seguinte:");
+            System.out.println(a + "x² - " + cPos + " = 0");
+        } else if ((a > 0 && b > 0 && c == 0)
+                || (a < 0 && b > 0 && c == 0)){
+            System.out.println("A equação é a seguinte:");
+            System.out.println(a + "x² + " + b + "x = 0");
+        } else if ((a > 0 && b > 0 && c > 0)
+                || (a < 0 && b > 0 && c > 0)){
+            System.out.println("A equação é a seguinte:");
+            System.out.println(a + "x² + " + b + "x + " + c + " = 0");
+        } else if ((a > 0 && b > 0 && c < 0)
+                || (a < 0 && b > 0 && c < 0)){
+            System.out.println("A equação é a seguinte:");
+            System.out.println(a + "x² + " + b + "x - " + cPos + " = 0");
+        } else if ((a > 0 && b < 0 && c == 0)
+                || (a < 0 && b < 0 && c == 0)){
+            System.out.println("A equação é a seguinte:");
+            System.out.println(a + "x² - " + bPos + "x" + " = 0");
+        } else if ((a > 0 && b < 0 && c > 0)
+                || (a < 0 && b < 0 && c > 0)){
+            System.out.println("A equação é a seguinte:");
+            System.out.println(a + "x² - " + bPos + "x + " + c + " = 0");
+        } else if ((a > 0 && b < 0 && c < 0)
+                || (a < 0 && b < 0 && c < 0)){
+            System.out.println("A equação é a seguinte:");
+            System.out.println(a + "x² - " + bPos + "x - " + cPos + " = 0");
+        }
     }
 }
